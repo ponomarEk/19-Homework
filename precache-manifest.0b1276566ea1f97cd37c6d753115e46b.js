172cfc9bbf5737fa14e90ae52977c14ff9b038a0
Sasha <ponomarek91@gmail.com>
2020-04-11 21:37:55 +0300

    Updates

---
+++ precache-manifest.0b1276566ea1f97cd37c6d753115e46b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3926ae5c08e1d6786132850a548dd86a",
+    "url": "/19-Homework/index.html"
+  },
+  {
+    "revision": "3a141974a341cc0ad521",
+    "url": "/19-Homework/static/css/main.789f2349.chunk.css"
+  },
+  {
+    "revision": "7020be40953e0ed11b97",
+    "url": "/19-Homework/static/js/2.70158bca.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/19-Homework/static/js/2.70158bca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a141974a341cc0ad521",
+    "url": "/19-Homework/static/js/main.2cb1ebda.chunk.js"
+  },
+  {
+    "revision": "7d99fbd76ce2968dfd53",
+    "url": "/19-Homework/static/js/runtime-main.f723c162.js"
+  },
+  {
+    "revision": "d964e0b3c644193e5591758f01e91f68",
+    "url": "/19-Homework/static/media/padlock.d964e0b3.svg"
+  }
+]);
